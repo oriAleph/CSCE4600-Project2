6cbe2ddedd38f7e52723638dcdebd274f3a1c934
xsilvos <61394062+xsilvos@users.noreply.github.com>
2023-05-11 21:45:43 -0500

    Add files via upload
    echo implementation

--- main.go
+++ main.go
@@ -89,6 +89,8 @@ func handleInput(w io.Writer, input string, exit chan<- struct{}) error {
 		return builtins.EnvironmentVariables(w, args...)
 	case "pwd":
 		return builtins.GetDirectory(w, args...)
+	case "echo":
+		return builtins.RepeatStuffs(args...)
 	case "exit":
 		exit <- struct{}{}
 		return nil
